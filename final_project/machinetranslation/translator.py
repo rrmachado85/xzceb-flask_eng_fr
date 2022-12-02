b9c7f3ef180bc2f1221c4255f8210c748bb1e4f3
rrmachado85 <rm.rodrigues.machado@gmail.com>
2022-12-02 06:07:57 -0500

    include instance language translator, englishToFrench, frenchToEnglish

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,3 +6,26 @@ from dotenv import load_dotenv
 load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
+version='2018-05-01'
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version=version,
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    frenchText = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    print(json.dumps(frenchText, indent=2, ensure_ascii=False))
+    return frenchText
+    
+def frenchToEnglish(frenchText):
+    englishText = language_translator.translate(
+    text=frenchText,
+    model_id='en-fr').get_result()
+    print(json.dumps(englishText, indent=2, ensure_ascii=False))
+    return englishText 
